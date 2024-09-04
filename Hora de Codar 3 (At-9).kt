58e2b0acea1df8c80cd2daf2d77848d217dc92d5
Anna Cristina <cristinaanna05@gmail.com>
2024-09-03 22:26:05 -0300

    Add files via upload

---
+++ Hora de Codar 3 (At-9).kt
@@ -0,0 +1,10 @@
+// -  Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100.
+fun main() {
+
+
+    var cont = 101
+    for (i in 1..10){
+        println(cont)
+        cont++
+    }
+    }
